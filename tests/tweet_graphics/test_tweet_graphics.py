4b45af5063c0e014cff9cb1a50a4e9dba820fd2e
José Fernando Costa <jose.fernando.costa.1998@gmail.com>
2020-08-30 22:26:48 +0100

    Improved/Added unit tests

--- tests/tweet_graphics/test_tweet_graphics.py
+++ tests/tweet_graphics/test_tweet_graphics.py
@@ -20,6 +20,9 @@ from .data_samples import (blue_mode_settings, dark_mode_settings,
                            invalid_username, invalid_wrap_limit,
                            light_mode_settings, missing_color_scheme,
                            missing_font_family, missing_font_size_header,
+                           missing_profile_pic_size,
+                           invalid_profile_pic_size_length,
+                           invalid_profile_pic_size_value,
                            missing_font_size_text, missing_margin,
                            missing_name, missing_pic, missing_size,
                            missing_tag, missing_text, missing_username,
@@ -48,45 +51,57 @@ def test_load_settings(mocker, format_chosen, return_settings):
     assert spy.spy_return == return_settings
 
 
-@pytest.mark.parametrize("custom_settings, default_format, expected_result", [
-    (valid_custom_settings, "", valid_custom_settings_returned),
-    ({}, "blue", blue_mode_settings_returned),
-    ({}, "light", light_mode_settings_returned),
-    ({}, "dark", dark_mode_settings_returned),
-    (valid_custom_settings, "blue", valid_custom_settings_returned),
-    (valid_custom_settings, "light", valid_custom_settings_returned),
-    (valid_custom_settings, "dark", valid_custom_settings_returned),
-    (valid_custom_settings_rgba, "", valid_custom_settings_rgba_returned),
-    (valid_custom_settings_none_bg, "", valid_custom_settings_none_bg_returned)
+@pytest.mark.parametrize("tweet_info, graphic_settings, default_format, expected_result", [
+    (valid_info_with_picture, valid_custom_settings,
+     "", valid_custom_settings_returned),
+    (valid_info_with_picture, {}, "blue", blue_mode_settings_returned),
+    (valid_info_with_picture, {}, "light", light_mode_settings_returned),
+    (valid_info_with_picture, {}, "dark", dark_mode_settings_returned),
+    (valid_info_with_picture, valid_custom_settings,
+     "blue", valid_custom_settings_returned),
+    (valid_info_with_picture, valid_custom_settings,
+     "light", valid_custom_settings_returned),
+    (valid_info_with_picture, valid_custom_settings,
+     "dark", valid_custom_settings_returned),
+    (valid_info_with_picture, valid_custom_settings_rgba,
+     "", valid_custom_settings_rgba_returned),
+    (valid_info_with_picture, valid_custom_settings_none_bg,
+     "", valid_custom_settings_none_bg_returned)
 ])
-def test_choose_settings_valid(mocker, custom_settings, default_format, expected_result):
-    spy = mocker.spy(src, "__choose_graphic_settings")
-    src.__choose_graphic_settings(custom_settings, default_format)
-    assert spy.spy_return == expected_result
+def test_choose_settings_valid(mocker, tweet_info, graphic_settings, default_format, expected_result):
+    settings_chosen = src.__choose_graphic_settings(
+        tweet_info,
+        graphic_settings,
+        default_format
+    )
+    assert settings_chosen == expected_result
 
 
-@pytest.mark.parametrize("custom_settings, default_format, expected_error", [
-    ({}, "", errors.MissingGraphicSettings),
-    (missing_font_family, "", errors.MissingDictKeys),
-    (missing_font_size_header, "", errors.MissingDictKeys),
-    (missing_font_size_text, "", errors.MissingDictKeys),
-    (missing_size, "", errors.MissingDictKeys),
-    (missing_color_scheme, "", errors.MissingDictKeys),
-    (missing_wrap_limit, "", errors.MissingDictKeys),
-    (missing_margin, "", errors.MissingDictKeys),
-    (invalid_font_family, "", errors.FontNotFound),
-    (invalid_font_size_header, "", TypeError),
-    (invalid_font_size_text, "", TypeError),
-    (invalid_size_length, "", errors.InvalidFieldLength),
-    (invalid_size_value, "", TypeError),
-    (invalid_color_scheme_length, "", errors.InvalidFieldLength),
-    (invalid_color_scheme_value, "", errors.InvalidColorFormat),
-    (invalid_wrap_limit, "", TypeError),
-    (invalid_margin_bottom, "", TypeError),
+@pytest.mark.parametrize("tweet_info, custom_settings, default_format, expected_error", [
+    (valid_info_with_picture, {}, "", errors.MissingGraphicSettings),
+    (valid_info_with_picture, missing_font_family, "", errors.MissingDictKeys),
+    (valid_info_with_picture, missing_font_size_header, "", errors.MissingDictKeys),
+    (valid_info_with_picture, missing_font_size_text, "", errors.MissingDictKeys),
+    (valid_info_with_picture, missing_size, "", errors.MissingDictKeys),
+    (valid_info_with_picture, missing_color_scheme, "", errors.MissingDictKeys),
+    (valid_info_with_picture, missing_wrap_limit, "", errors.MissingDictKeys),
+    (valid_info_with_picture, missing_margin, "", errors.MissingDictKeys),
+    (valid_info_with_picture, invalid_font_family, "", errors.FontNotFound),
+    (valid_info_with_picture, invalid_font_size_header, "", TypeError),
+    (valid_info_with_picture, invalid_font_size_text, "", TypeError),
+    (valid_info_with_picture, invalid_size_length, "", errors.InvalidFieldLength),
+    (valid_info_with_picture, invalid_size_value, "", TypeError),
+    (valid_info_with_picture, invalid_color_scheme_length,
+     "", errors.InvalidFieldLength),
+    (valid_info_with_picture, invalid_color_scheme_value,
+     "", errors.InvalidColorFormat),
+    (valid_info_with_picture, invalid_wrap_limit, "", TypeError),
+    (valid_info_with_picture, invalid_margin_bottom, "", TypeError),
 ])
-def test_choose_settings_invalid(mocker, custom_settings, default_format, expected_error):
+def test_choose_settings_invalid(mocker, tweet_info, custom_settings, default_format, expected_error):
     with pytest.raises(expected_error):
-        src.__choose_graphic_settings(custom_settings, default_format)
+        src.__choose_graphic_settings(
+            tweet_info, custom_settings, default_format)
 
 
 @pytest.mark.parametrize("graphic_info, graphic_settings, default_format, save_dir", [
@@ -106,7 +121,7 @@ def test_choose_settings_invalid(mocker, custom_settings, default_format, expect
     (valid_info_no_picture, {}, "light", ""),
     (valid_info_no_picture, {}, "light", "C:\\Users\\user\\Desktop"),
     (valid_info_no_picture, {}, "dark", ""),
-    (valid_info_no_picture, {}, "dark", "C:\\Users\\user\\Desktop"),
+    (valid_info_no_picture, {}, "dark", "C:\\Users\\user\\Desktop")
 ])
 def test_create_tweet(mocker, graphic_info, graphic_settings, default_format, save_dir):
     # Mock the `save` method
@@ -134,6 +149,7 @@ def test_create_tweet(mocker, graphic_info, graphic_settings, default_format, sa
     (valid_info_no_picture, missing_font_size_header, "", errors.MissingDictKeys),
     (valid_info_no_picture, missing_font_size_text, "", errors.MissingDictKeys),
     (valid_info_no_picture, missing_size, "", errors.MissingDictKeys),
+    (valid_info_no_picture, missing_profile_pic_size, "", errors.MissingDictKeys),
     (valid_info_no_picture, missing_color_scheme, "", errors.MissingDictKeys),
     (valid_info_no_picture, missing_wrap_limit, "", errors.MissingDictKeys),
     (valid_info_no_picture, missing_margin, "", errors.MissingDictKeys),
@@ -142,6 +158,9 @@ def test_create_tweet(mocker, graphic_info, graphic_settings, default_format, sa
     (valid_info_no_picture, invalid_font_size_text, "", TypeError),
     (valid_info_no_picture, invalid_size_length, "", errors.InvalidFieldLength),
     (valid_info_no_picture, invalid_size_value, "", TypeError),
+    (valid_info_no_picture, invalid_profile_pic_size_length,
+     "", errors.InvalidFieldLength),
+    (valid_info_no_picture, invalid_profile_pic_size_value, "", TypeError),
     (valid_info_no_picture, invalid_color_scheme_length, "", errors.InvalidFieldLength),
     (valid_info_no_picture, invalid_color_scheme_value, "", errors.InvalidColorFormat),
     (valid_info_no_picture, invalid_color_scheme_rgba, "", errors.InvalidColorFormat),
@@ -161,44 +180,95 @@ def test_create_graphic_fails(mocker, graphic_info, graphic_settings, default_fo
 
 
 @pytest.mark.parametrize("graphic_info, graphic_settings, expected_dimensions", [
-    (valid_info_no_picture, light_mode_settings, {"header": [530, 210.0], "text": [542, 92.0]}),
-    (valid_info_with_picture, light_mode_settings, {"header": [530, 210.0], "text": [542, 92.0]})
+    (valid_info_no_picture, valid_custom_settings,
+     {"header": [680, 210], "text": [542, 92]}),
+    (valid_info_with_picture, valid_custom_settings,
+     {"header": [680, 210], "text": [542, 92]})
 ])
 def test_calculate_content_dimensions(mocker, graphic_info, graphic_settings, expected_dimensions):
-    content_dimensions = utils.calculate_content_dimensions(graphic_info, light_mode_settings)
+    content_dimensions = utils.calculate_content_dimensions(
+        graphic_info, graphic_settings)
     assert content_dimensions == expected_dimensions
 
 
-def test_draw_header(mocker):
-    coords = (197, 602)
-    return_y = 837
-    dummy_img = Image.new("RGB", (0, 0), color="black")
-    draw = ImageDraw.Draw(dummy_img)
-    font_header = ImageFont.truetype("arial.ttf", 80, encoding="utf-8")
-    spy = mocker.spy(src, "__draw_header")
+@pytest.mark.parametrize("tweet_info, graphic_settings", [
+    (valid_info_no_picture, blue_mode_settings),
+    (valid_info_no_picture, dark_mode_settings),
+    (valid_info_no_picture, light_mode_settings)
+])
+def test_draw_header_without_profile_pic(mocker, tweet_info, graphic_settings):
+    coords = (620, 734)
+    return_y = 974
+    header_height = 210
 
-    src.__draw_header(
-        "test",
-        "@test",
-        "",
-        [1800, 1800],
+    img_size = graphic_settings["size"]
+    bg_color = graphic_settings["color_scheme"][0]
+    font_family = graphic_settings["font_family"]
+    font_size = graphic_settings["font_size_header"]
+
+    dummy_img = Image.new("RGB", img_size, color=bg_color)
+    draw = ImageDraw.Draw(dummy_img)
+    font_header = ImageFont.truetype(font_family, font_size, encoding="utf-8")
+    spy = mocker.spy(src, "__draw_header_without_profile_pic")
+
+    src.__draw_header_without_profile_pic(
+        tweet_info,
+        graphic_settings,
         dummy_img,
         draw,
         coords,
-        30,
-        font_header,
-        "black"
+        header_height,
+        font_header
     )
     assert spy.spy_return == return_y
 
 
-def test_get_initial_coords(mocker):
-    size = [1800, 1800]
-    dimensions = {'header': [770, 48.0], 'text': [1406, 548]}
-    return_coords = (197, 602)
+@pytest.mark.parametrize("tweet_info, graphic_settings", [
+    (valid_info_with_picture, valid_custom_settings),
+    (valid_info_with_picture, valid_custom_settings_rgba),
+    (valid_info_with_picture, valid_custom_settings_none_bg)
+])
+def test_draw_header_with_profile_pic(mocker, tweet_info, graphic_settings):
+    coords = (377, 732)
+    return_y = 956
+    header_height = 194
+
+    img_size = graphic_settings["size"]
+    profile_pic_size = graphic_settings["profile_pic_size"]
+    bg_color = graphic_settings["color_scheme"][0]
+    font_family = graphic_settings["font_family"]
+    font_size = graphic_settings["font_size_header"]
+
+    dummy_img = Image.new("RGB", img_size, color=bg_color)
+    dummy_profile_pic = Image.new("RGBA", profile_pic_size, color="black")
+    draw = ImageDraw.Draw(dummy_img)
+    font_header = ImageFont.truetype(font_family, font_size, encoding="utf-8")
+    spy = mocker.spy(src, "__draw_header_with_profile_pic")
+
+    src.__draw_header_with_profile_pic(
+        tweet_info,
+        graphic_settings,
+        dummy_img,
+        draw,
+        coords,
+        header_height,
+        font_header,
+        dummy_profile_pic
+    )
+    assert spy.spy_return == return_y
+
+
+@pytest.mark.parametrize("graphic_settings", [
+    (blue_mode_settings),
+    (dark_mode_settings),
+    (light_mode_settings)
+])
+def test_get_initial_coords(mocker, graphic_settings):
+    dimensions = {"header": [797.0, 194.0], "text": [1045, 111.0]}
+    return_coords = (377, 732)
     spy = mocker.spy(src, "__get_initial_coordinates")
 
-    src.__get_initial_coordinates(size, dimensions)
+    src.__get_initial_coordinates(graphic_settings, dimensions)
     assert spy.spy_return == return_coords
 
 
@@ -223,3 +293,45 @@ def test_validate_rgba_fails(mocker, g_settings):
     with pytest.raises(errors.InvalidColorFormat):
         validation.__validate_color_scheme(
             color_scheme, error_msg_size, error_msg_format)
+
+
+@pytest.mark.parametrize("graphic_settings", [
+    (blue_mode_settings_returned),
+    (dark_mode_settings_returned),
+    (light_mode_settings_returned),
+    (valid_custom_settings_returned),
+    (valid_custom_settings_rgba_returned),
+    (valid_custom_settings_none_bg_returned)
+])
+def test_create_graphic_fonts_header(mocker, graphic_settings):
+    font_family = graphic_settings["font_family"]
+    font_size = graphic_settings["font_size_header"]
+
+    font_header, _ = utils.create_graphic_fonts(graphic_settings)
+    # font_header_family = font_header.getname()[0]
+    font_header_size = font_header.size
+    # font_header_encoding = font_header.encoding
+
+    # assert font_family == font_header_family
+    assert font_size == font_header_size
+
+
+@pytest.mark.parametrize("graphic_settings", [
+    (blue_mode_settings_returned),
+    (dark_mode_settings_returned),
+    (light_mode_settings_returned),
+    (valid_custom_settings_returned),
+    (valid_custom_settings_rgba_returned),
+    (valid_custom_settings_none_bg_returned)
+])
+def test_create_graphic_fonts_text(mocker, graphic_settings):
+    font_family = graphic_settings["font_family"]
+    font_size = graphic_settings["font_size_text"]
+
+    _, font_text = utils.create_graphic_fonts(graphic_settings)
+    # font_text_family = font_text.getname()[0]
+    font_text_size = font_text.size
+    # font_text_encoding = font_text.encoding
+
+    # assert font_family == font_text_family
+    assert font_size == font_text_size
